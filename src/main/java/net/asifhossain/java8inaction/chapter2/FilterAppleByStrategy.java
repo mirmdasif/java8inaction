19bed1cffaed85baeb320015ddeb4062adbc0118
Mir Md. Asif Hossain <mir.md.asif@gmail.com>
2017-10-04 11:30:40 +0600

    Quiz 2.1

--- src/main/java/net/asifhossain/java8inaction/chapter2/FilterAppleByStrategy.java
+++ src/main/java/net/asifhossain/java8inaction/chapter2/FilterAppleByStrategy.java
@@ -12,6 +12,9 @@ public class FilterAppleByStrategy {
         System.out.println(filterApple(Bucket.getApples(), new GreenColorApplePredicate()));
         System.out.println(filterApple(Bucket.getApples(), new AppleHeavyWeightPredicate()));
 
+        prettyPrintApple(Bucket.getApples(), new SimpleAppleFormatter());
+        prettyPrintApple(Bucket.getApples(), new PrettyAppleFormatter());
+
     }
 
     interface ApplePredicate {
@@ -41,5 +44,30 @@ public class FilterAppleByStrategy {
         }
 
         return matchedApples;
+    }
+
+    interface AppleFormatter {
+        String format(Apple apple);
+    }
+
+    static class SimpleAppleFormatter implements AppleFormatter {
+        @Override
+        public String format(Apple apple) {
+            return apple.toString();
+        }
+    }
+
+    static class PrettyAppleFormatter implements AppleFormatter {
+        @Override
+        public String format(Apple apple) {
+            return "A" + (apple.getWeight() > 100 ? " heavy" : " light") + " " + apple.getColor() + " apple.";
+
+        }
+    }
+
+    static void  prettyPrintApple(List<Apple> apples, AppleFormatter formatter) {
+        for (Apple apple : apples) {
+            System.out.println(formatter.format(apple));
+        }
     }
 }
